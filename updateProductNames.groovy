6889aa08f3df2dad2b2e3591bb48ee6780523e81
Sarath Chandra <sarathchandrach@gmail.com>
2019-11-15 22:40:36 -0500

    Update updateProductNames.groovy

--- updateProductNames.groovy
+++ updateProductNames.groovy
@@ -1,4 +1,4 @@
 
-flexibleSearchService.search("select {p:pk} from {product as p} where {p:name} <> Null and {p:catalogy} = '8796224127577'").result.each {
+flexibleSearchService.search("select {pk} from {Product as p} where {p:name[en]:ol} IS NULL and {p:catalogVersion} in ('8796224127577')").result.each {
 
  }
