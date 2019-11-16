7dedcd6bb6db25bd8701b64a456ce52788583899
Sarath Chandra <sarathchandrach@gmail.com>
2019-11-15 22:53:46 -0500

    Update updateProductNames.groovy

--- updateProductNames.groovy
+++ updateProductNames.groovy
@@ -1,4 +1,13 @@
-
-flexibleSearchService.search("select {pk} from {Product as p} where {p:name[en]:ol} IS NULL and {p:catalogVersion} in ('8796224127577')").result.each {
-
+import de.hybris.platform.core.model.product.ProductModel;
+import java.util.Locale;
+import de.hybris.platform.servicelayer.search.FlexibleSearchQuery;
+Locale localeEn = new Locale("en");
+flexibleSearchService.search("select {pk} from {Product as p} where {p:name[en]:ol} IS NULL and {p:code}= 'CFFH17F1TW' and {p:catalogVersion} in ('8796224127577')").result.each {
+ 
+ query = "select {pk} from {Product as p} where {p:code} =" + it.getCode() + " and {p:catalogVersion} in ('8796126118489')";
+FlexibleSearchQuery fsquery = new FlexibleSearchQuery(query);
+  ProductModel product = flexibleSearchService.searchUnique(fsquery);
+  it.setName(product.getName(localeEn),localeEn);
+ modelService.save(it);
+ 
  }
